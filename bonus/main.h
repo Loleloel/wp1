8440846de64bf24ce39bfb0d283987cef8c562d4
Oliver Hansson <gushanolj@student.gu.se>
2026-01-29 22:53:48 +0100

    add brackets around expression

--- bonus/main.h
+++ bonus/main.h
@@ -7,7 +7,7 @@
 // To get the size in bits, we multiply the result from sizeof(int) by 8
 // Finally, as an int is signed by default, we subtract 1 to ensure
 // this bit isn't included, as it could lead to undefined behavior.
-#define INT_SIZE_IN_BITS sizeof(int) * 8 - 1
+#define INT_SIZE_IN_BITS (sizeof(int) * 8 - 1)
 
 int checkArgs(char *arg);
 void countBits(int);
